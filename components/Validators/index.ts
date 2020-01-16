9fbd84f13d84f7fa91ed8b53c9cb004e32464d87
HauptmannEck <eckhoffj@gmail.com>
2020-01-15 20:50:09 -0600

    fix missing validations

--- components/Validators/index.ts
+++ components/Validators/index.ts
@@ -2,10 +2,16 @@ import required from './required';
 import phone from './phone';
 import email from './email';
 import custom from './custom';
+import percent from './percent';
+import positive from './positive';
+import number from './number';
 
 export {
     required,
     phone,
     email,
     custom,
+    number,
+    percent,
+    positive,
 };
